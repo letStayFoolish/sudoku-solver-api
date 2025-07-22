e58130117fd04b9ef731bcdddf6311703ec7c611
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-07-22 16:18:32 +0200

    Add basic puzzle solving logic to SudokuService

--- Services/SudokuService.cs
+++ Services/SudokuService.cs
@@ -22,7 +22,7 @@ public class SudokuService : ISudokuService
     };
     // Here you'd generate a puzzle based on the `difficulty`.
     // But for now, we return the hard-coded grid for testing.
-    return easyPuzzle;
+    return SolvePuzzle(easyPuzzle) ? easyPuzzle : newGrid;
   }
 
   private int[,] _GenerateDefaultGrid()
@@ -37,24 +37,54 @@ public class SudokuService : ISudokuService
     {
       for (var col = 0; col < 9; col++)
       {
-        int[] cellIndex = [row, col];
-        
         if (grid[row, col] == 0)
         {
           // do something...
-          for (int i = 1; i <= 9; i++)
+          for (int num = 1; num <= 9; num++)
           {
-            // check if i is already in column
+            if (_IsSafe(grid, row, col, num))
+            {
+              // if all of conditions from above are satisfied, place a number and keep going through the grid
+              grid[row, col] = num;
 
-            // check if i is already in row
-
-            // check if i is already in section
+              // Recursive call to solve the next cell
+              if (SolvePuzzle(grid))
+                return true;
+              // Reset cell if no solution is found
+              grid[row, col] = 0;
+            }
           }
+
+          return false;
         }
       }
     }
-    
-    throw new NotImplementedException();
+
+    return true; // Puzzle is fully solved
+  }
+
+  private bool _IsSafe(int[,] grid, int row, int col, int num)
+  {
+    // check if num is already in column
+    // check if i is already in row
+    for (int i = 0; i < 9; i++)
+    {
+      if (grid[row, i] == num || grid[i, col] == num)
+        return false;
+    }
+
+    // check if i is already in the 3x3 subgrid
+    int startRow = row - (row % 3), startCol = col - (col % 3);
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        if (grid[startRow + i, startCol + i] == num)
+          return false;
+      }
+    }
+
+    return true;
   }
 
   public bool IsValidSolution(int[,] originalGrid, int[,] userGrid)
